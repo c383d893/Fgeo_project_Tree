ac7363df5493de82f4453b9ee8f7e343d98d09fd
c383d893 <66577803+c383d893@users.noreply.github.com>
2023-04-28 16:05:48 +0200

    Delete functions directory

--- functions/dispersal2.R
+++
@@ -1,8 +0,0 @@
-# If seed disperses our of edge of focal area, it comes back through the opposite side
-dispersal.fun2 = function(loc, xlim = plotwidth, ylim = plotheight, alpha) {	# Recruits disperse across plot edges in a torus
-  
-  test = dispersal.fun(loc[1], loc[2], alpha = alpha, n = 1)
-  test[,1] = test[,1] %% xlim		# Torus
-  test[,2] = test[,2] %% ylim
-  return(data.frame(x1 = test[,1], y1 = test[,2]))
-}
